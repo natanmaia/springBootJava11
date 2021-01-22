06b2121a0b502df0f0022c44bef855297c55b74f
natanmaia <natanmaia_17@hotmail.com>
2021-01-22 15:46:53 -0300

    classes category e book

---
+++ src/main/java/com/estudo/spring/model/Book.java
@@ -0,0 +1,79 @@
+package com.estudo.spring.model;
+
+import java.util.Objects;
+
+public class Book {
+
+    private Integer id;
+    private String tittle;
+    private String author;
+    private String text;
+
+    private Category category;
+
+    public Book(){
+        super();
+    }
+
+    public Book(Integer id, String tittle, String author, String text, Category category) {
+        super();
+        this.id = id;
+        this.tittle = tittle;
+        this.author = author;
+        this.text = text;
+        this.category = category;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        Book book = (Book) o;
+        return getId().equals(book.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
---
+++ src/main/java/com/estudo/spring/model/Category.java
@@ -0,0 +1,71 @@
+package com.estudo.spring.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Category {
+
+    private Integer id;
+    private String name;
+    private String description;
+
+    private List<Book> books = new ArrayList<>();
+
+
+    public Category() {
+        super();
+    }
+
+    public Category(Integer id, String name, String description) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category)) return false;
+        Category category = (Category) o;
+        return getId().equals(category.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
